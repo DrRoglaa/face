7183709de1664724a4d0ea2af5f28d9558800581
David Rok Roglič <davidrok@itsroglic.com>
2023-03-08 22:15:29 +0100

    changed logo added css

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Tilt from 'react-parallax-tilt';
-import brain from './brain.jpg';
+import brain from './brain.png';
+import './Logo.css';
 
 const Logo = () => {
         return (
             <Tilt>
-            <div style={{ width: '300px', backgroundColor: 'darkgreen' }}>
-            <p><img alt='brain' src={brain} /></p>
+            <div class name='br2 shadow-2' style={{ width: '150px'}}>
+            <p className='pa3'><img style={{paddingTop: '5px'}}alt='brain' src={brain}/></p>
             </div>
           </Tilt>
     );
