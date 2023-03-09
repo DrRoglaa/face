4fc249f2f49c0aba0c2a48a136a1d24f15bf9c52
David Rok Roglič <davidrok@itsroglic.com>
2023-03-09 21:05:23 +0100

    static rank added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import './App.css';
 import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Rank from './Components/Rank/Rank';
+import ParticlesBg from 'particles-bg'
 import 'tachyons';
+
 
 class App extends Component{
   render(){
   return (
     <div className="App">
+      <ParticlesBg type="cobweb" bg={true}/>
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       { /*
       <FaceRecognition/> */ }
